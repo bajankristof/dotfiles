39f170262f694382ba816bbeb6b38e4fbaecdc9b
bajankristof <bajankristof@gmail.com>
2025-07-07 21:06:37 +0200

    feat: show macro recording events in lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,8 +2,20 @@ return {
   'nvim-lualine/lualine.nvim',
   event = 'VeryLazy',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function ()
+  config = function()
     local lualine = require('lualine')
+
+    vim.api.nvim_create_autocmd('RecordingEnter', {
+      callback = function()
+        lualine.refresh { place = { 'statusline' } }
+      end
+    })
+
+    vim.api.nvim_create_autocmd('RecordingLeave', {
+      callback = function()
+        lualine.refresh { place = { 'statusline' } }
+      end
+    })
 
     lualine.setup {
       options = {
@@ -12,8 +24,28 @@ return {
         section_separators = { left = '', right = '' },
       },
       sections = {
-        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-        lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 } },
+        lualine_a = {
+          {
+            'mode',
+            separator = { left = '' },
+            right_padding = 2
+          },
+          {
+            'macro',
+            fmt = function()
+              local reg = vim.fn.reg_recording()
+              if reg == '' then return '' end
+              return 'recording @' .. reg
+            end,
+          },
+        },
+        lualine_z = {
+          {
+            'location',
+            separator = { right = '' },
+            left_padding = 2
+          },
+        },
       },
     }
   end
