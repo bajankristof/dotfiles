5decdef219df339ea55ecfdd613222596d5e5e4b
bajankristof <bajankristof@gmail.com>
2025-11-26 09:57:49 +0100

    refact: better OS specific project structure

--- setup/stow.zsh
+++ setup/stow.zsh
@@ -7,7 +7,12 @@ cd "${0:a:h}"
 source colors.zsh
 
 blue 'Installing dotfiles...'
-stow -d .. -t "$HOME" .
+stow -d .. -t "$HOME" common
+if [[ $OSTYPE = 'darwin'* ]]; then
+  stow -d .. -t "$HOME" macos
+else
+  stow -d .. -t "$HOME" arch
+fi
 green 'done'
 echo ''
 
