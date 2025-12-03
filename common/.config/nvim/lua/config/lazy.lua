ef3cf25bdbb12c008d1e37f5a01199f70029d6a3
bajankristof <bajankristof@gmail.com>
2025-12-03 09:13:45 +0100

    chore(common): check for Neovim plugin updates once a week

--- common/.config/nvim/lua/config/lazy.lua
+++ common/.config/nvim/lua/config/lazy.lua
@@ -23,8 +23,6 @@ vim.opt.rtp:prepend(bootstrap())
 
 require('lazy').setup({
   spec = {{ import = 'plugins' }},
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { 'habamax' } },
-  checker = { enabled = true },
+  checker = { enabled = true, frequency = 7 * 24 * 60 * 60 },
 })
